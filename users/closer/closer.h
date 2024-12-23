ef5d668877a60241bd953bd8d88f64dea4517283
closer <closer@users.noreply.github.com>
2024-12-23 18:11:28 +0900

    update

--- users/closer/closer.h
+++ users/closer/closer.h
@@ -13,14 +13,3 @@ tap_dance_action_t tap_dance_actions[] = {
   [TD_GUIH] = ACTION_TAP_DANCE_MOD_DOUBLE(KC_LGUI, KC_LNG2, KC_LNG1),
   [SPCCMDT] = ACTION_TAP_DANCE_TRIPLE(KC_SPC, KC_COMM, KC_DOT)
 };
-
-// Tapping Term Per Key
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // IMEの切り替え
-    case KC_GUIH:
-      return 200;
-    default:
-      return TAPPING_TERM;
-  }
-}
