3199ea76386be8e3c53e744fda33f8cf958fc802
closer <closer@users.noreply.github.com>
2024-12-26 11:03:41 +0900

    update

--- users/closer/closer.h
+++ users/closer/closer.h
@@ -7,8 +7,6 @@ enum {
   SPCCMDT
 };
 
-#define KC_GUIH TD(TD_GUIH)
-
 tap_dance_action_t tap_dance_actions[] = {
   [TD_GUIH] = ACTION_TAP_DANCE_MOD_DOUBLE(KC_LGUI, KC_LNG2, KC_LNG1),
   [SPCCMDT] = ACTION_TAP_DANCE_TRIPLE(KC_SPC, KC_COMM, KC_DOT)
